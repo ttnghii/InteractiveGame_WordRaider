26d196a4e62e7c1fbc780e46e2ca69bbca675ca3
ttnghii <takoyaki1402@gmail.com>
2024-12-18 13:23:09 +0700

    moew moew

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-with open('words.txt', 'r') as file:
-    words = [line.strip() for line in file]
+import random
 
-# Create an empty list to conserve all words
+
+# Build the word bank and reading it
+with open('words.txt', 'r') as file:
+    words = [line.strip().lower() for line in file]
+
 bank = []
 for word in words:
     bank.append(word)
+
+word_to_guess = random.choice(bank)
+
+
+# Variables to Track game state
+misplaced_guesses, incorrect_guesses = [], []
+max_turn = 5   # set defualt
+current_turn = 0
+
+## Print the current fame state
+print('-'*10, 'Welcome to Word Raider game', '-'*10)
+print('This word has', len(word_to_guess), 'letters.')
+if (max_turn - current_turn) > 1: 
+    print('Now, you have', max_turn - current_turn, 'turns left.')
+else:
+    print('Now, you have', max_turn - current_turn, 'turn left.')
